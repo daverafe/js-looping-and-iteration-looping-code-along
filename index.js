31f4af714177028a4f47295cb0707b4162419932
David Rafe <david.rafe@gmail.com>
2021-05-12 20:37:01 -0400

    commit tree beda3eedc02073eebbd8b8de15f490bfacc5379b

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arr, event) {
+    const thankYou = []
+    for (let i = 0; i < arr.length; i++) {
+        thankYou.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYou 
+}
+
+
+function countDown(int) {
+    while (int >= 0) {
+        console.log(int--)
+    }
+}
